26bd1708d417b6fa9322faed0dce013d9e6d3364
Abduljaleel89 <jalil.hijazy@gmail.com>
2023-12-08 04:12:47 +0500

    ..

--- js/gallery.js
+++ js/gallery.js
@@ -5,8 +5,9 @@ function upDate(previewPic) {
        2) Change the text  of the div with the id = "image" 
        to the alt text of the preview image 
        */
-    document.getElementById('image').innerHTML = previewPic.alt;
-    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
+    document.getElementById('header_img').innerHTML = previewPic.alt;
+    document.getElementById('header_img').style.backgroundImage = "url(" + previewPic.src + ")";
+    console.log("done");
 }
 
 function unDo() {
@@ -17,6 +18,20 @@ function unDo() {
     2) Change the text  of the div with the id = "image" 
     back to the original text.  You can use the html code to see what that original text was
     */
-    document.getElementById('image').style.backgroundImage = "url('')"
-    document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
+    document.getElementById('header_img').style.backgroundImage = "url('')"
+    document.getElementById('header_img').innerHTML = 'Hover over an image below to display here.';
+    console.log("UNdone");
 }
+/*function pickImage(){
+    options = ["car1.jpg", "car2.jpeg", "car3.jpeg", "car4.jpeg", "car5.jpeg", "car6.jpeg"]
+    for (var i = 0; i < options; i++) {
+        console.log("image "+ i)
+    }
+    randomImg = "img/" + options[Math.floor(Math.random() * options.length)];
+        img = document.querySelector("#header_img")
+        img.setAttribute("src", randomImg)
+        img.setAttribute("alt", "")
+        img.setAttribute("tabindex", "0")
+    console.log("updated");
+
+}*/
